c9bc14d12f465175a45a057d2486508edb1f6673
DMonMac <dmmacaso@gmail.com>
2017-09-18 01:49:28 +0800

    breakdown only shows items with quantity

--- react-pos-2/src/App.js
+++ react-pos-2/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 
 class Item extends Component {
+
   render(){
     return (
       <div className="col col-md-3 col-xs-12">
@@ -21,29 +22,30 @@ class Item extends Component {
         <div className="caption">
           <h5>{this.props.name}</h5>
           <p>Price: ${this.props.price}</p>
+          <p>Qty: {this.props.qty}</p>
         </div>
       </div>
     )
   }
 }
 
-class List extends Component {
-  render(){
-    return (
-      <div>
+//class List extends Component {
+//  render(){
+//    return (
+//      <div>
 
-      </div>
-    )
-  }
-}
+//      </div>
+//    )
+//  }
+//}
 
 class Breakdown extends Component {
   render(){
     return (
-      <div>
-      Item Name (Qty) = $ Price*Qty <button>-qty by 1</button>
-      <h4>Total: $ Total Price</h4>
-      </div>
+          <div>
+            <button>-</button>
+            {this.props.name} ({this.props.qty}) = $ {this.props.price*this.props.qty}
+          </div>
     )
   }
 }
@@ -56,11 +58,11 @@ class App extends Component {
     this.state={
       price_total:0,
       item_db: [
-        {name: "Andrew Lloyd", price: 213, img_id: "01", qty: 0},
+        {name: "Andrew Lloyd", price: 213, img_id: "01", qty: 1},
         {name: "eyeFone Ate", price: 1234, img_id: "02", qty: 0},
-        {name: "Sam Tsung", price: 123, img_id: "03", qty: 0},
+        {name: "Sam Tsung", price: 123, img_id: "03", qty: 2},
         {name: "CopyKo", price: 12, img_id: "04", qty: 0},
-        {name: "Cher", price: 142, img_id: "05", qty: 0}
+        {name: "Cher", price: 142, img_id: "05", qty: 3}
       ]
     };
     this.calcTotal = this.calcTotal.bind(this);
@@ -70,7 +72,9 @@ class App extends Component {
     this.setState({price_total: this.state.price_total + price});
   }
 
+
   render() {
+    var func = this;
     var item_buttons = this.state.item_db.map(
       function(item){
         return(
@@ -78,8 +82,23 @@ class App extends Component {
             name = {item.name}
             price = {item.price}
             img_id = {item.img_id}
+            qty = {item.qty}
+            calcTotal = {func.calcTotal}
           />
         );
+      }
+    );
+    var item_breakdown = this.state.item_db.map(
+      function(item){
+        if (item.qty > 0) {
+          return(
+            <Breakdown
+              name = {item.name}
+              price = {item.price}
+              qty = {item.qty}
+            />
+          );
+        }
       }
     );
     return (
@@ -96,12 +115,14 @@ class App extends Component {
         <div className="row">
           <div className="col-4">
             <h3>Breakdown</h3>
-            <Breakdown/>
+            {item_breakdown}
+            <h4>Total: $ {this.state.price_total}</h4>
           </div>
           <div className="col-8">
             <h3>Item List</h3>
             <div className = "row">
               {item_buttons}
+
             </div>
           </div>
         </div>
