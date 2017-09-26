85f9bb4c7a1a22ab366a8567bb462113d4569bfa
DMonMac <dmmacaso@gmail.com>
2017-09-26 11:09:59 +0800

    pretty much customized the look for public APIs

--- pub-api/src/App.js
+++ pub-api/src/App.js
@@ -10,12 +10,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Public APIs</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <API breed="dingo"></API>
+        <div>
+          <API breed="samoyed"></API>
+        </div>
       </div>
     );
   }
