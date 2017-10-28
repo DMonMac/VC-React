7e62a09f4ee09fcf8dfc38b7261161a0dda70e11
DMonMac <dmmacaso@gmail.com>
2017-10-28 19:36:47 +0800

    Added table for data

--- tutorials/api_auth/src/App.js
+++ tutorials/api_auth/src/App.js
@@ -7,12 +7,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      apiData: {}
+      apiData: {},
+      data: []
     }
     //this.callAPIjson = this.callAPIjson.bind(this);
   }
 
-  componentDidMount(){ // note: if you add () it executes the function, without it it copies the cintent into the return
+  componentWillMount(){ // note: if you add () it executes the function, without it it copies the cintent into the return
     let url = "http://api.demo.muulla.com/cms/merchant/all/active/10/1"
     fetch(url, {
       method: 'get',
@@ -22,25 +23,62 @@ class App extends Component {
     })
       .then(api => api.json())
       .then(apiJSON => {
-        this.setState({
-          apiData: apiJSON
-        })
-        console.log(this.state.apiData)
+        this.setState(
+          { apiData: apiJSON }, () => {
+            this.setState(
+              { data: this.state.apiData.data }
+            )
+          }
+        )
+
       });
   }
+
   render() {
+    console.log(this.state.apiData)
+    let data_list = this.state.data.map((datum, index) => {
+      return (
+        <tr key={index}>
+          <img src={datum.logo.path_to_file} alt='No Logo'/>
+          <td>{datum.display_name}</td>
+          <td>{datum.email}</td>
+          <td>{datum.phone}</td>
+          <td>{datum.status}</td>
+          <td>
+            <ol>
+              <li>{datum.address.address1}</li>
+              <li>{datum.address.address2}</li>
+              <li>{datum.address.address3}</li>
+            </ol>
+          </td>
+        </tr>
+      )
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Authorized API</h1>
-        </header>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Authorized API</h1>
+          </header>
+        </div>
+        <table >
+          <tr>
+            <th><h2>Image</h2></th>
+            <th><h2>Display Name</h2></th>
+            <th><h2>Email</h2></th>
+            <th><h2>Phone</h2></th>
+            <th><h2>Status</h2></th>
+            <th><h2>Address</h2></th>
+          </tr>
+          {data_list}
+        </table>
+
       </div>
+
     );
   }
 }
 
 export default App;
-
-//{this.callAPIjson()}
-//{console.log(this.state.apiData)}
